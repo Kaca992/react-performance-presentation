d575d3d4022fd87da2c27deea65f2c949e7fed07
kacavendadino <kacavendadino@gmail.com>
2019-05-08 19:58:42 +0200

    redux removed

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import configureStore from './store';
-import { Provider } from 'react-redux';
 import {BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom';
 import { MutationPage } from './2. Props Mutation';
 import { ChildrenPage } from './3. Children and pure component';
 import GhostPropsContainer from './4. Ghost props';
 
-const store = configureStore();
-
 ReactDOM.render(
-<Provider store={store}>
     <Router>
         <Switch>
             <Route path={"/example-2"} component={MutationPage} />
@@ -21,5 +16,4 @@ ReactDOM.render(
                 <Redirect to="/example-2" />
             </Route>
         </Switch>
-    </Router>
-</Provider>, document.getElementById('root'));
+    </Router>, document.getElementById('root'));
